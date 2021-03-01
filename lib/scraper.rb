be9fd6963aae9b7201247dedd524e2b5ba3219ca
pbenton6 <patrick.benton6396@gmail.com>
2021-03-01 10:23:46 -0600

    test

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-require_relative './player.rb'
+require_relative './team.rb'
 
 class Scraper
     
     def get_page
-        Nokogiri::HTML(open())
+        Nokogiri::HTML(open("https://www.msn.com/en-us/sports/mlb/teams"))
     end
 end
