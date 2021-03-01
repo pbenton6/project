9292e6ad47c6ba92d55283974abb94dd3914e6dd
pbenton6 <patrick.benton6396@gmail.com>
2021-03-01 07:48:32 -0600

    added scraper

---
+++ lib/team.rb
@@ -0,0 +1,19 @@
+class Team
+    
+    
+
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+
+end
